467125fa66af34a31e188b71fc40e55ad79f4245
regygu <ugyan.gergely@gmail.com>
2017-05-05 11:41:36 +0200

    server setup

--- src/GCDataGenerator.java
+++ src/GCDataGenerator.java
@@ -1,5 +1,4 @@
-/**
- * Created by ugyan on 2017.05.05..
- */
+
 public class GCDataGenerator {
+
 }
--- src/GreenCommitmentServer.java
+++ src/GreenCommitmentServer.java
@@ -1,5 +1,22 @@
-/**
- * Created by ugyan on 2017.05.05..
- */
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class GreenCommitmentServer {
+
+    int port;
+    ServerSocket server;
+    Socket socket;
+    InputStreamReader in;
+
+
+    public GreenCommitmentServer() throws IOException {
+        this.port = 9090;
+        this. server = new ServerSocket(port);
+        this.socket = server.accept();
+        this.in = new InputStreamReader(socket.getInputStream());
+    }
+
+
 }
